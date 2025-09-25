3072237af30dca5230f3d0d9cc86938c906d2154
unknown <muriloeduborges@gmail.com>
2025-09-25 11:31:28 -0300

    [feat]

--- App.js
+++ App.js
@@ -4,9 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>2 Ano</Text>
-      <Text>3 Bimestre</Text>
-      <Text>Adriel B. Estevan</Text>
+      <Text style={styles.text}>2 Ano</Text>
+      <Text style={styles.text}>3 Bimestre</Text>
+      <Text style={styles.text}>Adriel B. Estevan</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,5 +18,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
